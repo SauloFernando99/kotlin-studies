b696d7655355dd34257e2a3269d9e5f6ebe02b53
SauloFernando99 <saulofernando114@gmail.com>
2024-02-24 10:39:28 -0300

    feat: solve problem by using a generic high-order-function

--- pdm-kotlin-exercises/src/Exercise4/Exercise4.kt
+++ pdm-kotlin-exercises/src/Exercise4/Exercise4.kt
@@ -2,6 +2,13 @@ package Exercise4
 
 //Generic high-order-function
 
+fun soma(i: Int, j: Int) = i + j
+fun cat(i: String, j: String) = "${i}${j}"
+
+fun <I, J, F> xpto(param1: I, param2: J, callback: (I, J) -> F): F {
+    return callback(param1, param2)
+}
+
 fun main() {
     println(xpto(2, 3, ::soma))
     println(xpto("Jo", "ão", ::cat))
