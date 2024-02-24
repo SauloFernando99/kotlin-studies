fd0c6150cebd60bd5394fd926cd2bd005985ae43
SauloFernando99 <saulofernando114@gmail.com>
2024-02-24 09:53:13 -0300

    feat: function to find the strings that contains the string Sistemas

---
+++ pdm-kotlin-exercises/src/exercise1.kt
@@ -0,0 +1,27 @@
+//Find Sistemas in string
+
+fun findSistemasString(list: MutableList<String>): MutableList<String> {
+
+    val foundStrings = mutableListOf<String>()
+
+    for (string in list) {
+        if ("Sistemas" in string) {
+            foundStrings.add(string)
+        }
+    }
+
+    return foundStrings
+}
+
+fun main(){
+    val listaCursos: MutableList<String> = mutableListOf(
+        "Sistemas para Dispositivos Móveis",
+        "Análise e Desevolvimento de Sistemas"
+    )
+
+    listaCursos.add("Técnico em Informática para Internet")
+    listaCursos.add("Manutenção de Aeronaves")
+    listaCursos.add("Técnico em Células")
+    listaCursos.add("Processos Gerenciais")
+
+}
