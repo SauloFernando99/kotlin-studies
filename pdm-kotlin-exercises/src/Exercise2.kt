f2a328adc56f6e56a9c82227f90e9b23ec728127
SauloFernando99 <saulofernando114@gmail.com>
2024-02-24 10:09:29 -0300

    feat: solving problem by adding a for with a print with key/value for each Pair in familiaMap

---
+++ pdm-kotlin-exercises/src/Exercise2.kt
@@ -0,0 +1,14 @@
+//Map print
+
+fun main(){
+    val familiaMap: MutableMap<String, String> = mutableMapOf(Pair("Pai", "Pedro"),
+        Pair("Mãe", "Marcela"),
+        Pair("Filho", "João"),
+        Pair("Caçula", "Cadu"),
+        Pair("Pet", "Paçoca"))
+
+    for (familyMember in familiaMap){
+        println(familyMember.key + ": " + familyMember.value)
+    }
+
+}
